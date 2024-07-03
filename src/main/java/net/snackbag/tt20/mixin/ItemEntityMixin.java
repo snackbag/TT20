a3a083808ecb100c7986578d0c247d5ab954d580
JXSnack <jx.snack@gmail.com>
2024-07-03 20:01:02 +0200

    item drop time

---
+++ src/main/java/net/snackbag/tt20/mixin/ItemEntityMixin.java
@@ -0,0 +1,18 @@
+package net.snackbag.tt20.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.entity.ItemEntity;
+import net.snackbag.tt20.TT20;
+import net.snackbag.tt20.util.TPSUtil;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ItemEntity.class)
+public class ItemEntityMixin {
+    @ModifyExpressionValue(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/ItemEntity;pickupDelay:I", opcode = Opcodes.GETFIELD))
+    private int pickupDelayTT20(int original) {
+        if (!TT20.config.enabled()) return original;
+        return TPSUtil.tt20(original, true);
+    }
+}

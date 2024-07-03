09f90955b2a481db76189f4200180e75c48c15c1
JXSnack <jx.snack@gmail.com>
2024-07-03 17:15:38 +0200

    use main config

--- src/main/java/net/snackbag/tt20/TT20.java
+++ src/main/java/net/snackbag/tt20/TT20.java
@@ -3,6 +3,7 @@ package net.snackbag.tt20;
 import net.fabricmc.api.ModInitializer;
 
 import net.snackbag.tt20.command.CommandRegistry;
+import net.snackbag.tt20.config.MainConfig;
 import net.snackbag.tt20.util.TPSCalculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class TT20 implements ModInitializer {
 	public static final TPSCalculator TPS_CALCULATOR = new TPSCalculator();
 
 	public static boolean enabled = true;
+	public static final MainConfig config = new MainConfig();
 
 	@Override
 	public void onInitialize() {

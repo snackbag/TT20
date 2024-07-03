da984595572533a0e70f53c17eef850b4a97125e
JXSnack <jx.snack@gmail.com>
2024-07-03 15:01:08 +0200

    create TPS calculator

--- src/main/java/net/snackbag/TT20.java
+++ src/main/java/net/snackbag/TT20.java
@@ -2,21 +2,20 @@ package net.snackbag;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.text.Text;
+import net.snackbag.util.TPSCalculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TT20 implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("tt20");
+	public static final String VERSION = "0.1.0";
+	public static final TPSCalculator TPS_CALCULATOR = new TPSCalculator();
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Starting TT20...");
 	}
 }
---
+++ src/main/java/net/snackbag/util/TPSCalculator.java
@@ -0,0 +1,37 @@
+package net.snackbag.util;
+
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+
+public class TPSCalculator {
+    public Long lastTick;
+    public Long currentTick;
+
+    public TPSCalculator() {
+        ServerTickEvents.START_SERVER_TICK.register(server -> onTick());
+    }
+
+    private void onTick() {
+        if (currentTick != null) {
+            lastTick = currentTick;
+        }
+
+        currentTick = System.currentTimeMillis();
+    }
+
+    public long getMSPT() {
+        return currentTick - lastTick;
+    }
+
+    public long getTPS() {
+        if (lastTick == null) {
+            return -1;
+        }
+
+        long tps = 1000 / getMSPT();
+        if (tps > 20) {
+            return 20;
+        } else {
+            return tps;
+        }
+    }
+}

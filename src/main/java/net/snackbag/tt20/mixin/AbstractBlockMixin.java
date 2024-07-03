3fe368503759feafac47edd4ad39d8c6e754dc42
JXSnack <jx.snack@gmail.com>
2024-07-03 16:41:52 +0200

    fix block lag

---
+++ src/main/java/net/snackbag/tt20/mixin/AbstractBlockMixin.java
@@ -0,0 +1,23 @@
+package net.snackbag.tt20.mixin;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import net.snackbag.tt20.TT20;
+import net.snackbag.tt20.util.TPSUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractBlock.class)
+public class AbstractBlockMixin {
+    @Inject(method = "calcBlockBreakingDelta", at = @At("RETURN"), cancellable = true)
+    private void onBlockBreakingCalc(BlockState state, PlayerEntity player, BlockView world, BlockPos pos, CallbackInfoReturnable<Float> cir) {
+        if (cir.getReturnValue() != 0.0f) {
+            cir.setReturnValue(TPSUtil.tt20(cir.getReturnValue()));
+        }
+    }
+}
---
+++ src/main/java/net/snackbag/tt20/mixin/ServerPlayerInteractionManagerMixin.java
@@ -0,0 +1,33 @@
+package net.snackbag.tt20.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
+import net.minecraft.server.network.ServerPlayerInteractionManager;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
+import net.snackbag.tt20.TT20;
+import net.snackbag.tt20.util.TPSUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ServerPlayerInteractionManager.class)
+public class ServerPlayerInteractionManagerMixin {
+    @Shadow private int tickCounter;
+
+    @Shadow private int startMiningTime;
+
+    @Redirect(method = "processBlockBreakingAction", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;calcBlockBreakingDelta(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)F"))
+    private float onProcessBlockBreakingAction(BlockState instance, PlayerEntity playerEntity, BlockView blockView, BlockPos blockPos) {
+        float v = instance.calcBlockBreakingDelta(playerEntity, playerEntity.getWorld(), blockPos);
+        int j = tickCounter - startMiningTime;
+        return v + TPSUtil.tt20(j);
+    }
+}

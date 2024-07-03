c4b2560b6e90e538c8af0f9a293287803ab30a41
JXSnack <jx.snack@gmail.com>
2024-07-03 18:01:32 +0200

    better descriptor

--- src/main/java/net/snackbag/tt20/mixin/DedicatedServerWatchdogMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/DedicatedServerWatchdogMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(DedicatedServerWatchdog.class)
 public class DedicatedServerWatchdogMixin {
     @ModifyExpressionValue(method = "run", at = @At(value = "FIELD", target = "Lnet/minecraft/server/dedicated/DedicatedServerWatchdog;maxTickTime:J", opcode = Opcodes.GETFIELD))
-    private long onGetMaxTickTime(long original) {
+    private long cancelServerWatchdog(long original) {
         return TT20.config.serverWatchdog() ? original : Long.MAX_VALUE; // using Long.MAX_VALUE, because we can't cancel the if statement
     }
 }

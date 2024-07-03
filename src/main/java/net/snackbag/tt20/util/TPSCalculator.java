9b3cc6d01e8dc45048b673b27f57ecc1c3d9e726
JXSnack <jx.snack@gmail.com>
2024-07-03 21:54:00 +0200

    add TPS history and average

--- src/main/java/net/snackbag/tt20/util/TPSCalculator.java
+++ src/main/java/net/snackbag/tt20/util/TPSCalculator.java
@@ -2,9 +2,15 @@ package net.snackbag.tt20.util;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalDouble;
+
 public class TPSCalculator {
     public Long lastTick;
     public Long currentTick;
+    private List<Double> tpsHistory = new ArrayList<>();
+    private static final int historyLimit = 100;
 
     public TPSCalculator() {
         ServerTickEvents.START_SERVER_TICK.register(server -> onTick());
@@ -16,10 +22,27 @@ public class TPSCalculator {
         }
 
         currentTick = System.currentTimeMillis();
+        addToHistory(getTPS());
+    }
+
+    private void addToHistory(double tps) {
+        if (tpsHistory.size() >= historyLimit) {
+            tpsHistory.remove(historyLimit - 1);
+        }
+
+        tpsHistory.add(tps);
     }
 
     private long getMSPT() {
         return currentTick - lastTick;
+    }
+
+    public double getAverageTPS() {
+        OptionalDouble average = tpsHistory
+                .stream()
+                .mapToDouble(a -> a)
+                .average();
+        return average.isPresent() ? average.getAsDouble() : 0.1;
     }
 
     public double getTPS() {

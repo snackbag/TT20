a63b4e91b871649cc9bd60608e30b9712c826e7e
JXSnack <jx.snack@gmail.com>
2024-07-03 17:55:33 +0200

    stop watchdog if disabled

---
+++ src/main/java/net/snackbag/tt20/mixin/DedicatedServerWatchdogMixin.java
@@ -0,0 +1,16 @@
+package net.snackbag.tt20.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.server.dedicated.DedicatedServerWatchdog;
+import net.snackbag.tt20.TT20;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(DedicatedServerWatchdog.class)
+public class DedicatedServerWatchdogMixin {
+    @ModifyExpressionValue(method = "run", at = @At(value = "FIELD", target = "Lnet/minecraft/server/dedicated/DedicatedServerWatchdog;maxTickTime:J", opcode = Opcodes.GETFIELD))
+    private long onGetMaxTickTime(long original) {
+        return TT20.config.serverWatchdog() ? original : Long.MAX_VALUE; // using Long.MAX_VALUE, because we can't cancel the if statement
+    }
+}

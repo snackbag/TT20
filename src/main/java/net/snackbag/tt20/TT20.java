b4ceb30bb9183a8562156b4ccf47373f3a67c796
JXSnack <jx.snack@gmail.com>
2024-07-03 22:17:18 +0200

    forgot to bump version

--- src/main/java/net/snackbag/tt20/TT20.java
+++ src/main/java/net/snackbag/tt20/TT20.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 
 public class TT20 implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("tt20");
-	public static final String VERSION = "0.3.1";
+	public static final String VERSION = "0.4.0";
 	public static final TPSCalculator TPS_CALCULATOR = new TPSCalculator();
 
 	public static final MainConfig config = new MainConfig();

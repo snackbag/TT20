82e1c44234fa0453d18b32c61b8e50cd27f8c36f
JXSnack <jx.snack@gmail.com>
2024-07-04 23:16:00 +0200

    fix sleeping times

--- src/main/java/net/snackbag/tt20/mixin/PlayerEntityMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/PlayerEntityMixin.java
@@ -1,12 +1,13 @@
 package net.snackbag.tt20.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.entity.player.PlayerEntity;
 import net.snackbag.tt20.TT20;
 import net.snackbag.tt20.util.TPSUtil;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
@@ -16,5 +17,11 @@ public class PlayerEntityMixin {
         if (original == 1) return original;
 
         return TPSUtil.tt20(original, false);
+    }
+
+    @ModifyExpressionValue(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerEntity;sleepTimer:I", opcode = Opcodes.GETFIELD))
+    private int tickTT20(int original) {
+        if (!TT20.config.enabled()) return original;
+        return original + TT20.TPS_CALCULATOR.applicableMissedTicks();
     }
 }

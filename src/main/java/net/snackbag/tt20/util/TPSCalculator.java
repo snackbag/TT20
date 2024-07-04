fd0382b3f1220c6b5c346bb5cfc12008a742c481
JXSnack <jx.snack@gmail.com>
2024-07-04 11:15:21 +0200

    missed tick logic

--- src/main/java/net/snackbag/tt20/util/TPSCalculator.java
+++ src/main/java/net/snackbag/tt20/util/TPSCalculator.java
@@ -8,8 +8,12 @@ import java.util.List;
 public class TPSCalculator {
     public Long lastTick;
     public Long currentTick;
+    private double allMissedTicks = 0;
     private final List<Double> tpsHistory = new ArrayList<>();
     private static final int historyLimit = 40;
+
+    private static final int MAX_TPS = 20;
+    private static final int FULL_TICK = 50;
 
     public TPSCalculator() {
         ServerTickEvents.START_SERVER_TICK.register(server -> onTick());
@@ -22,6 +26,8 @@ public class TPSCalculator {
 
         currentTick = System.currentTimeMillis();
         addToHistory(getTPS());
+        clearMissedTicks();
+        missedTick();
     }
 
     private void addToHistory(double tps) {
@@ -48,10 +54,34 @@ public class TPSCalculator {
         if (getMSPT() <= 0) return 0.1;
 
         double tps = 1000 / (double) getMSPT();
-        return tps > 20 ? 20 : tps;
+        return tps > MAX_TPS ? MAX_TPS : tps;
+    }
+
+    public void missedTick() {
+        if (lastTick == null) return;
+
+        long mspt = getMSPT() <= 0 ? 50 : getMSPT();
+        double missedTicks = (mspt / (double) FULL_TICK) - 1;
+        allMissedTicks += missedTicks <= 0 ? 0 : missedTicks;
     }
 
     public double getMostAccurateTPS() {
         return getTPS() > getAverageTPS() ? getAverageTPS() : getTPS();
+    }
+
+    public double getAllMissedTicks() {
+        return allMissedTicks;
+    }
+
+    public int applicableMissedTicks() {
+        return (int) Math.floor(allMissedTicks);
+    }
+
+    public void clearMissedTicks() {
+        allMissedTicks -= applicableMissedTicks();
+    }
+
+    public void resetMissedTicks() {
+        allMissedTicks = 0;
     }
 }

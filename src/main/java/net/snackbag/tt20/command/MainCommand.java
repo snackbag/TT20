105b5bd368719cf40a79a158787d589659d8d1dc
JXSnack <jx.snack@gmail.com>
2024-07-04 12:52:17 +0200

    add status command

--- src/main/java/net/snackbag/tt20/command/MainCommand.java
+++ src/main/java/net/snackbag/tt20/command/MainCommand.java
@@ -16,10 +16,25 @@ public class MainCommand {
             CommandManager.RegistrationEnvironment environment) {
         dispatcher.register(
                 CommandManager.literal("tt20").executes(MainCommand::executeMain)
-                        .then(CommandManager.literal("toggle").requires(source -> source.hasPermissionLevel(3)).executes(MainCommand::executeToggle))
                         .then(CommandManager.literal("tps").executes(MainCommand::executeTps))
+                        .then(CommandManager.literal("status").executes(MainCommand::executeStatus))
+                        .then(CommandManager.literal("toggle").requires(source -> source.hasPermissionLevel(3)).executes(MainCommand::executeToggle))
                         .then(CommandManager.literal("reload").requires(source -> source.hasPermissionLevel(2)).executes(MainCommand::executeReload))
         );
+    }
+
+    private static int executeStatus(CommandContext<ServerCommandSource> context) {
+        var source = context.getSource();
+
+        source.sendMessage(Text.literal("§7TT20 enabled: " + (TT20.config.enabled() ? "§aON" : "§cOFF")));
+        source.sendMessage(Text.literal("§7Block entity acceleration: " + (TT20.config.blockEntityAcceleration() ? "§aON" : "§cOFF")));
+        source.sendMessage(Text.literal("§7Server watchdog: " + (TT20.config.serverWatchdog() ? "§aON" : "§cOFF")));
+        executeTps(context, false);
+        source.sendMessage(Text.literal("\n§8Version: §7" + TT20.VERSION));
+        source.sendMessage(Text.literal("§8MSPT: §7" + TT20.TPS_CALCULATOR.getMSPT()));
+        source.sendMessage(Text.literal("§8Missed ticks: §7" + TT20.TPS_CALCULATOR.getAllMissedTicks()));
+
+        return 1;
     }
 
     private static int executeReload(CommandContext<ServerCommandSource> context) {
@@ -32,6 +47,10 @@ public class MainCommand {
     }
 
     private static int executeTps(CommandContext<ServerCommandSource> context) {
+        return executeTps(context, true);
+    }
+
+    private static int executeTps(CommandContext<ServerCommandSource> context, boolean missedTicks) {
         var source = context.getSource();
 
         source.sendMessage(Text.literal(
@@ -39,7 +58,8 @@ public class MainCommand {
                         "§7 with average " + TPSUtil.colorizeTPS(TT20.TPS_CALCULATOR.getAverageTPS(), true) +
                         "§7 accurate " + TPSUtil.colorizeTPS(TT20.TPS_CALCULATOR.getMostAccurateTPS(), true)
         ));
-        source.sendMessage(Text.literal("§8Missed ticks: §7" + TT20.TPS_CALCULATOR.getAllMissedTicks()));
+
+        if (missedTicks) source.sendMessage(Text.literal("§8Missed ticks: §7" + TT20.TPS_CALCULATOR.getAllMissedTicks()));
 
         return 1;
     }

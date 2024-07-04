c891627244f6c3d25b7a89e4edf1e7fa61e70b0c
JXSnack <jx.snack@gmail.com>
2024-07-04 12:34:49 +0200

    better config loading

--- src/main/java/net/snackbag/tt20/config/MainConfig.java
+++ src/main/java/net/snackbag/tt20/config/MainConfig.java
@@ -1,16 +1,16 @@
 package net.snackbag.tt20.config;
 
 public class MainConfig extends JSONConfiguration {
-    private boolean enabled;
-    private boolean blockEntityAcceleration;
-    private boolean serverWatchdog;
+    private boolean enabled = true;
+    private boolean blockEntityAcceleration = false;
+    private boolean serverWatchdog = true;
 
     public MainConfig() {
         super("config.json");
 
-        putIfEmpty("enabled", true);
-        putIfEmpty("block-entity-acceleration", false);
-        putIfEmpty("server-watchdog", true);
+        putIfEmpty("enabled", enabled);
+        putIfEmpty("block-entity-acceleration", blockEntityAcceleration);
+        putIfEmpty("server-watchdog", serverWatchdog);
 
         save();
     }
@@ -18,9 +18,10 @@ public class MainConfig extends JSONConfiguration {
     @Override
     public void reload() {
         super.reload();
-        this.enabled = getAsBoolean("enabled");
-        this.blockEntityAcceleration = getAsBoolean("block-entity-acceleration");
-        this.serverWatchdog = getAsBoolean("block-entity-acceleration");
+
+        this.enabled = getAsBooleanOrDefault("enabled", enabled);
+        this.blockEntityAcceleration = getAsBooleanOrDefault("block-entity-acceleration", blockEntityAcceleration);
+        this.serverWatchdog = getAsBooleanOrDefault("block-entity-acceleration", serverWatchdog);
     }
 
     public void enabled(boolean enabled) {

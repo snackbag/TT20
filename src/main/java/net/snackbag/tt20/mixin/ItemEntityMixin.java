ea30c76e3af4512aacc9a82ae6af208ea6e08357
JXSnack <jx.snack@gmail.com>
2024-07-04 22:11:58 +0200

    remove debug logging

--- src/main/java/net/snackbag/tt20/mixin/ItemEntityMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/ItemEntityMixin.java
@@ -13,7 +13,6 @@ public class ItemEntityMixin {
     @ModifyExpressionValue(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/ItemEntity;pickupDelay:I", opcode = Opcodes.GETFIELD))
     private int pickupDelayTT20(int original) {
         if (!TT20.config.enabled()) return original;
-        TT20.LOGGER.info(original + " " + TPSUtil.tt20(original, true));
         return Math.min(TPSUtil.tt20(original, true), 3);
     }
 }

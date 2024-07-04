a071a5352490902482694fd2b3a34a3d4558e8fd
JXSnack <jx.snack@gmail.com>
2024-07-04 12:30:38 +0200

    add blockEntityAcceleration config

--- src/main/java/net/snackbag/tt20/config/MainConfig.java
+++ src/main/java/net/snackbag/tt20/config/MainConfig.java
@@ -1,13 +1,26 @@
 package net.snackbag.tt20.config;
 
 public class MainConfig extends JSONConfiguration {
+    private boolean enabled;
+    private boolean blockEntityAcceleration;
+    private boolean serverWatchdog;
+
     public MainConfig() {
         super("config.json");
 
         putIfEmpty("enabled", true);
+        putIfEmpty("block-entity-acceleration", false);
         putIfEmpty("server-watchdog", true);
 
         save();
+    }
+
+    @Override
+    public void reload() {
+        super.reload();
+        this.enabled = getAsBoolean("enabled");
+        this.blockEntityAcceleration = getAsBoolean("block-entity-acceleration");
+        this.serverWatchdog = getAsBoolean("block-entity-acceleration");
     }
 
     public void enabled(boolean enabled) {
@@ -15,7 +28,7 @@ public class MainConfig extends JSONConfiguration {
     }
 
     public boolean enabled() {
-        return getAsBoolean("enabled");
+        return enabled;
     }
 
     public void serverWatchdog(boolean enabled) {
@@ -23,6 +36,14 @@ public class MainConfig extends JSONConfiguration {
     }
 
     public boolean serverWatchdog() {
-        return getAsBoolean("server-watchdog");
+        return serverWatchdog;
+    }
+
+    public void blockEntityAcceleration(boolean enabled) {
+        put("block-entity-acceleration", enabled);
+    }
+
+    public boolean blockEntityAcceleration() {
+        return blockEntityAcceleration;
     }
 }

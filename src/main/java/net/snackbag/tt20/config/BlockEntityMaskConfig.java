b6f8ef9548aa194554e986b9fbee05e29cd37831
JXSnack <jx.snack@gmail.com>
2024-07-04 14:36:14 +0200

    add getMask method to config

--- src/main/java/net/snackbag/tt20/config/BlockEntityMaskConfig.java
+++ src/main/java/net/snackbag/tt20/config/BlockEntityMaskConfig.java
@@ -1,14 +1,21 @@
 package net.snackbag.tt20.config;
 
+import net.minecraft.registry.Registries;
+import net.snackbag.tt20.util.Mask;
+
 public class BlockEntityMaskConfig extends JSONConfiguration {
     public BlockEntityMaskConfig() {
         super("block_entity_mask.json");
 
-        String[] defaultMask = {"minecraft:*"};
+        String[] defaultMask = {"*:*"};
 
         putIfEmpty("type", "whitelist");
         putIfEmpty("blocks", defaultMask);
 
         save();
+    }
+
+    public Mask getMask() {
+        return new Mask(Registries.BLOCK, this, "blocks");
     }
 }

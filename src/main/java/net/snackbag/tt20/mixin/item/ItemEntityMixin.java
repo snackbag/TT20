dfead30348de880c14cc5b6d3f9e9b792e305014
JXSnack <jx.snack@gmail.com>
2024-07-05 20:19:57 +0200

    actually fix pickup delay now

--- src/main/java/net/snackbag/tt20/mixin/item/ItemEntityMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/item/ItemEntityMixin.java
@@ -6,13 +6,26 @@ import net.snackbag.tt20.TT20;
 import net.snackbag.tt20.util.TPSUtil;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntity.class)
 public class ItemEntityMixin {
-    @ModifyExpressionValue(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/ItemEntity;pickupDelay:I", opcode = Opcodes.GETFIELD))
-    private int pickupDelayTT20(int original) {
-        if (!TT20.config.enabled()) return original;
-        return Math.min(TPSUtil.tt20(original, true), 3);
+    @Shadow private int pickupDelay;
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void pickupDelayTT20(CallbackInfo ci) {
+        if (!TT20.config.enabled()) return;
+
+        if (pickupDelay == 0) return;
+
+        if (pickupDelay - TT20.TPS_CALCULATOR.applicableMissedTicks() <= 0) {
+            pickupDelay = 0;
+            return;
+        }
+
+        pickupDelay = pickupDelay - TT20.TPS_CALCULATOR.applicableMissedTicks();
     }
 }

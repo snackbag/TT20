03ac986a48edb8f5d3a67c7d57b0db1f40f495d1
Bawnorton <bawnorton@me.com>
2024-07-07 10:06:04 +0100

    cleanup and improve stability of mixins

--- src/main/java/net/snackbag/tt20/mixin/PlayerEntityMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/PlayerEntityMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(PlayerEntity.class)
-public class PlayerEntityMixin {
+public abstract class PlayerEntityMixin {
     //? if <=1.20.6 {
     @ModifyReturnValue(method = "getMaxNetherPortalTime", at = @At("RETURN"))
     private int netherPortalTimeTT20(int original) {
--- src/main/java/net/snackbag/tt20/mixin/world/WorldChunkMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/world/WorldChunkMixin.java
@@ -1,5 +1,7 @@
 package net.snackbag.tt20.mixin.world;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
@@ -10,19 +12,18 @@ import net.minecraft.world.chunk.WorldChunk;
 import net.snackbag.tt20.TT20;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(WorldChunk.DirectBlockEntityTickInvoker.class)
-public class WorldChunkMixin {
-    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/entity/BlockEntityTicker;tick(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/block/entity/BlockEntity;)V"))
-    private <T extends BlockEntity> void onTick(BlockEntityTicker<T> instance, World world, BlockPos blockPos, BlockState blockState, T t) {
-        instance.tick(world, blockPos, blockState, t);
+public abstract class WorldChunkMixin {
+    @WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/entity/BlockEntityTicker;tick(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/block/entity/BlockEntity;)V"))
+    private <T extends BlockEntity> void onTick(BlockEntityTicker<T> instance, World world, BlockPos blockPos, BlockState blockState, BlockEntity blockEntity, Operation<Void> original) {
+        original.call(instance, world, blockPos, blockState, blockEntity);
         if (!TT20.config.enabled()) return;
         if (!TT20.config.blockEntityAcceleration()) return;
         if (!TT20.blockEntityMaskConfig.getMask().isOkay(Registries.BLOCK.getId(blockState.getBlock()))) return;
 
         for (int i = 0; i < TT20.TPS_CALCULATOR.applicableMissedTicks(); i++) {
-            instance.tick(world, blockPos, blockState, t);
+            original.call(instance, world, blockPos, blockState, blockEntity);
         }
     }
 }

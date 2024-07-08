224aa656c9caaca1faa418a33feef0ad9f8d97af
JXSnack <jx.snack@gmail.com>
2024-07-08 23:29:16 +0200

    isClient check for AbstractBlockMixin

--- src/main/java/net/snackbag/tt20/mixin/world/AbstractBlockMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/world/AbstractBlockMixin.java
@@ -1,7 +1,9 @@
 package net.snackbag.tt20.mixin.world;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.entity.player.PlayerEntity;
 import net.snackbag.tt20.TT20;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,8 +11,9 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(AbstractBlock.class)
 public abstract class AbstractBlockMixin {
     @ModifyReturnValue(method = "calcBlockBreakingDelta", at = @At("RETURN"))
-    private float onBlockBreakingCalc(float original) {
+    private float onBlockBreakingCalc(float original, @Local PlayerEntity player) {
         if (!TT20.config.enabled() || !TT20.config.blockBreakingAcceleration()) return original;
+        if (player.getWorld().isClient()) return original;
         return original * 20 / (float) TT20.TPS_CALCULATOR.getMostAccurateTPS();
     }
 }

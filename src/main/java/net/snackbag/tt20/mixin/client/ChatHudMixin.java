dddc3b02cb85055c848186a0002723039718a59f
JXSnack <jx.snack@gmail.com>
2024-07-11 15:15:10 +0200

    [1.19 port] fix warning

--- src/main/java/net/snackbag/tt20/mixin/client/ChatHudMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/client/ChatHudMixin.java
@@ -2,8 +2,10 @@ package net.snackbag.tt20.mixin.client;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+//? if >=1.20.1
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.snackbag.tt20.TT20;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,9 +18,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ChatHud.class)
 public abstract class ChatHudMixin {
     @Shadow public abstract void addMessage(Text message);
-
     @Inject(method = "render", at = @At("HEAD"))
-    //? if >=1.20.5 {
+    //? if <=1.19.2 {
+    /*private void onRender(MatrixStack matrices, int currentTick, CallbackInfo ci) {
+    *///?} elif >=1.20.5 {
     /*private void onPlayerConnectWarn(DrawContext context, int currentTick, int mouseX, int mouseY, boolean focused, CallbackInfo ci) {
     *///?} else {
     private void onPlayerConnectWarn(DrawContext context, int currentTick, int mouseX, int mouseY, CallbackInfo ci) {

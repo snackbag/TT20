45c100588e108efde5b3c45674a29ac89f56ba29
JXSnack <jx.snack@gmail.com>
2024-08-07 14:58:15 +0200

    use MAX_TPS value instead of hardcoded 20

--- src/main/java/net/snackbag/tt20/mixin/world/AbstractBlockMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/world/AbstractBlockMixin.java
@@ -5,6 +5,7 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.snackbag.tt20.TT20;
+import net.snackbag.tt20.util.TPSCalculator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -14,6 +15,6 @@ public abstract class AbstractBlockMixin {
     private float onBlockBreakingCalc(float original, @Local PlayerEntity player) {
         if (!TT20.config.enabled() || !TT20.config.blockBreakingAcceleration()) return original;
         if (player.getWorld().isClient()) return original;
-        return original * 20 / (float) TT20.TPS_CALCULATOR.getMostAccurateTPS();
+        return original * TPSCalculator.MAX_TPS / (float) TT20.TPS_CALCULATOR.getMostAccurateTPS();
     }
 }

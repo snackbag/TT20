95ffbb7bbd650c5f9d5b8c0170791006bfad176c
JXSnack <jx.snack@gmail.com>
2024-08-07 15:08:09 +0200

    better automatic updater text

--- src/main/java/net/snackbag/tt20/command/MainCommand.java
+++ src/main/java/net/snackbag/tt20/command/MainCommand.java
@@ -43,7 +43,7 @@ public class MainCommand {
         source.sendMessage(Text.literal("\n§8Version: §7" + TT20.VERSION));
         source.sendMessage(Text.literal("§8MSPT: §7" + TT20.TPS_CALCULATOR.getMSPT()));
         source.sendMessage(Text.literal("§8Missed ticks: §7" + TPSUtil.formatMissedTicks(TT20.TPS_CALCULATOR.getAllMissedTicks())));
-        source.sendMessage(Text.literal("§8Automatic updater: §7" + (TT20.config.automaticUpdater() ? "§atrue" : "§cfalse")));
+        source.sendMessage(Text.literal("§8Automatic updater: §7" + (TT20.config.automaticUpdater() ? "§aenabled" : "§cdisabled")));
 
         return 1;
     }

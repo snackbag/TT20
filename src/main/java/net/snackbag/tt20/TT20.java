8fa83903e396d37152f4de5875c8830f447506a5
JXSnack <jx.snack@gmail.com>
2024-12-29 22:18:55 +0100

    update version

--- src/main/java/net/snackbag/tt20/TT20.java
+++ src/main/java/net/snackbag/tt20/TT20.java
@@ -13,7 +13,7 @@ import java.util.concurrent.CompletableFuture;
 
 public class TT20 implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("tt20");
-	public static final String VERSION = "0.7.0";
+	public static final String VERSION = "0.7.1";
 	public static final TPSCalculator TPS_CALCULATOR = new TPSCalculator();
 	public static final MainConfig config = new MainConfig();
 	public static final BlockEntityMaskConfig blockEntityMaskConfig = new BlockEntityMaskConfig();

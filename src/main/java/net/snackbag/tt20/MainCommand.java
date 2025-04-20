6579d339a0df8e801f9ed149ca744271351c577c
Lemonnik6484 <LemonGamer6484@gmail.com>
2025-04-20 23:43:18 +0200

    Main Class & Config

---
+++ src/main/java/net/snackbag/tt20/MainCommand.java
@@ -0,0 +1,94 @@
+package net.snackbag.tt20;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+import net.snackbag.tt20.util.TPSUtil;
+
+public class MainCommand {
+    public MainCommand(CommandDispatcher<CommandSourceStack> dispatcher) {
+        dispatcher.register(
+                Commands.literal("tt20")
+                        .then(Commands.literal("tps").executes(ctx -> executeTps(ctx, true)))
+                        .then(Commands.literal("status").executes(MainCommand::executeStatus))
+        );
+    }
+
+    private static int executeTps(CommandContext<CommandSourceStack> ctx, boolean missedTicks) {
+        CommandSourceStack source = ctx.getSource();
+        source.sendSuccess(() -> Component.literal(
+                "§7TPS " + TPSUtil.colorizeTPS(TT20.TPS_CALCULATOR.getTPS(), true) +
+                        "§7 with average " + TPSUtil.colorizeTPS(TT20.TPS_CALCULATOR.getAverageTPS(), true) +
+                        "§7 accurate " + TPSUtil.colorizeTPS(TT20.TPS_CALCULATOR.getMostAccurateTPS(), true)
+        ), false);
+
+        if (missedTicks) {
+            source.sendSuccess(() -> Component.literal(
+                    "§8Missed ticks: §7" + TPSUtil.formatMissedTicks(TT20.TPS_CALCULATOR.getAllMissedTicks())
+            ), false);
+        }
+        return 1;
+    }
+
+    private static int executeStatus(CommandContext<CommandSourceStack> ctx) {
+        CommandSourceStack source = ctx.getSource();
+        source.sendSuccess(() -> Component.literal(
+                "§7TT20 enabled: " + (Config.ENABLED.get() ? "§aON" : "§cOFF")
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§7Block entity acceleration: " + (Config.BLOCK_ENTITY_ACCELERATION.get() ? "§aON" : "§cOFF")
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§7Block breaking acceleration: " + (Config.BLOCK_BREAKING_ACCELERATION.get() ? "§aON" : "§cOFF")
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§7Potion effect acceleration: " + (Config.POTION_EFFECT_ACCELERATION.get() ? "§aON" : "§cOFF")
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§7Fluid acceleration: " + (Config.FLUID_ACCELERATION.get() ? "§aON" : "§cOFF")
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§7Pickup acceleration: " + (Config.PICKUP_ACCELERATION.get() ? "§aON" : "§cOFF")
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§7Eating acceleration: " + (Config.EATING_ACCELERATION.get() ? "§aON" : "§cOFF")
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§7Portal acceleration: " + (Config.PORTAL_ACCELERATION.get() ? "§aON" : "§cOFF")
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§7Sleeping acceleration: " + (Config.SLEEPING_ACCELERATION.get() ? "§aON" : "§cOFF")
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§7Time acceleration: " + (Config.TIME_ACCELERATION.get() ? "§aON" : "§cOFF")
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§7Random tickspeed acceleration: " + (Config.RANDOM_TICKSPEED_ACCELERATION.get() ? "§aON" : "§cOFF")
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§7Singleplayer warning: " + (Config.SINGLEPLAYER_WARNING.get() ? "§aON" : "§cOFF")
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§7Server watchdog: " + (Config.SERVER_WATCHDOG.get() ? "§aON" : "§cOFF")
+        ), false);
+
+        executeTps(ctx, false);
+
+        source.sendSuccess(() -> Component.literal(
+                "\n§8Version: §7" + TT20.VERSION
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§8MSPT: §7" + TT20.TPS_CALCULATOR.getMSPT()
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§8Missed ticks: §7" + TPSUtil.formatMissedTicks(TT20.TPS_CALCULATOR.getAllMissedTicks())
+        ), false);
+        source.sendSuccess(() -> Component.literal(
+                "§8Automatic updater: §7" + (Config.AUTOMATIC_UPDATER.get() ? "§aenabled" : "§cdisabled")
+        ), false);
+
+        return 1;
+    }
+}
---
+++ src/main/java/net/snackbag/tt20/TT20.java
@@ -0,0 +1,58 @@
+package net.snackbag.tt20;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.server.command.ConfigCommand;
+import org.slf4j.Logger;
+
+import net.snackbag.tt20.util.TPSCalculator;
+
+@Mod(TT20.MODID)
+public class TT20 {
+    public static final String MODID = "tt20";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String VERSION = "0.7.1";
+    public static final TPSCalculator TPS_CALCULATOR = new TPSCalculator();
+
+    public TT20(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(TPS_CALCULATOR);
+
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Starting TT20...");
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            TT20.LOGGER.warn("******* WARNING *******");
+            TT20.LOGGER.warn("(TT20) Mod is running on a client instead of server, this is not recommended and will lead to errors.");
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = TT20.MODID)
+    public static class CommandRegistrationHandler {
+        @SubscribeEvent
+        public static void onCommandsRegister(RegisterCommandsEvent event) {
+            new MainCommand(event.getDispatcher());
+            ConfigCommand.register(event.getDispatcher());
+        }
+    }
+
+}

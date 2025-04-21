4ac4289dfd9bed460c8c026c1217864351c76535
Lemonnik6484 <LemonGamer6484@gmail.com>
2025-04-21 17:08:24 +0200

    Fluid Acceleration

---
+++ src/main/java/net/snackbag/tt20/mixin/fluid/LavaFluidMixin.java
@@ -0,0 +1,17 @@
+package net.snackbag.tt20.mixin.fluid;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.world.level.material.LavaFluid;
+import net.snackbag.tt20.TT20;
+import net.snackbag.tt20.util.TPSUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(LavaFluid.class)
+public class LavaFluidMixin {
+    @ModifyReturnValue(method = "getTickDelay", at = @At("RETURN"))
+    private int tickRateTT20(int original) {
+        if (!TT20.config.enabled() || !TT20.config.fluidAcceleration()) return original;
+        return TPSUtil.tt20(original, true);
+    }
+}
---
+++ src/main/java/net/snackbag/tt20/mixin/fluid/WaterFluidMixin.java
@@ -0,0 +1,17 @@
+package net.snackbag.tt20.mixin.fluid;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.world.level.material.WaterFluid;
+import net.snackbag.tt20.TT20;
+import net.snackbag.tt20.util.TPSUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(WaterFluid.class)
+public class WaterFluidMixin {
+    @ModifyReturnValue(method = "getTickDelay", at = @At("RETURN"))
+    private int tickRateTT20(int original) {
+        if (!TT20.config.enabled() || !TT20.config.fluidAcceleration()) return original;
+        return TPSUtil.tt20(original, true);
+    }
+}

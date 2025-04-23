d456e409c6f28ef61eeb190d43821fa6d313196f
Lemonnik6484 <LemonGamer6484@gmail.com>
2025-04-23 18:10:55 +0200

    toggle with integrated server start

--- src/main/java/net/snackbag/tt20/mixin/client/IntegratedServerMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/client/IntegratedServerMixin.java
@@ -8,10 +8,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Environment(EnvType.CLIENT)
 @Mixin(IntegratedServer.class)
 public abstract class IntegratedServerMixin {
+    @Inject(method = "setupServer", at = @At("HEAD"))
+    private void disableTT20(CallbackInfoReturnable<Boolean> cir) {
+        TT20.config.enabled(TT20.config.singlePlayerEnabled());
+    }
+
     @Inject(method = "stop", at = @At("HEAD"))
     private void resetWarn(boolean waitForShutdown, CallbackInfo ci) {
         TT20.warned = false;

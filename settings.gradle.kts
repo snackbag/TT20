fe0c42cd81a8ab88c5ded0cd18a8ea3e573bf44a
JXSnack <jx.snack@gmail.com>
2025-07-31 11:46:10 +0200

    update stonecutter and move to blahaj

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.architectury.dev")
+        maven("https://maven.minecraftforge.net")
+        maven("https://maven.neoforged.net/releases/")
+        maven("https://maven.kikugie.dev/snapshots")
+        maven("https://maven.kikugie.dev/releases")
+        maven("https://maven.txni.dev/releases")
+    }
+}
+
+plugins {
+    id("dev.architectury.loom") version "1.10-SNAPSHOT" apply false
+    kotlin("jvm") version "2.0.0" apply false
+    kotlin("plugin.serialization") version "2.0.0" apply false
+    id("toni.blahaj") version "2.0.17"
+    id("dev.kikugie.stonecutter") version "0.6-alpha.5"
+}
+
+blahaj {
+    init(rootProject) {
+        mc("1.20.1", "fabric", "forge")
+        mc("1.21.1", "fabric", "neoforge")
+        mc("1.21.4", "fabric")
+        mc("1.21.5", "fabric")
+    }
+}
+
+rootProject.name = settings.extra["mod.name"] as String
---
+++ stonecutter.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("dev.kikugie.stonecutter")
+    id("toni.blahaj")
+}
+
+stonecutter active "1.20.1-fabric" /* [SC] DO NOT EDIT */

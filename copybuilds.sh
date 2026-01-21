cb09da82fb6c70dd34a4ba4644668910ff0dbdc8
lemonnik <lemongamer6484@gmail.com>
2026-01-21 19:00:39 +0100

    new copybuilds script
    (cherry picked from commit 77af277ab324d841fd5ab7187faca07cdf09cb2a)

--- copybuilds.sh
+++ copybuilds.sh
@@ -1,11 +1,30 @@
-# script to copy build files into one public-builds directory
+#!/usr/bin/env bash
+set -e
 
-mkdir -p publish-builds
-cp versions/1.19.2/build/libs/tt20-0.7.2.jar publish-builds/tt20-0.7.2+mc1.19.2.jar
-cp versions/1.20.1/build/libs/tt20-0.7.2.jar publish-builds/tt20-0.7.2+mc1.20.1.jar
-cp versions/1.20.2/build/libs/tt20-0.7.2.jar publish-builds/tt20-0.7.2+mc1.20.2.jar
-cp versions/1.20.3/build/libs/tt20-0.7.2.jar publish-builds/tt20-0.7.2+mc1.20.3.jar
-cp versions/1.20.5/build/libs/tt20-0.7.2.jar publish-builds/tt20-0.7.2+mc1.20.5.jar
-cp versions/1.21/build/libs/tt20-0.7.2.jar publish-builds/tt20-0.7.2+mc1.21.jar
-cp versions/1.21.2/build/libs/tt20-0.7.2.jar publish-builds/tt20-0.7.2+mc1.21.2.jar
-cp versions/1.21.9/build/libs/tt20-0.7.2.jar publish-builds/tt20-0.7.2+mc1.21.9.jar
+mod_id="$(grep -E '^mod_id=' gradle.properties | cut -d'=' -f2)"
+mod_version="$(grep -E '^mod_version=' gradle.properties | cut -d'=' -f2)"
+
+if [ -z "$mod_version" ]; then
+  echo "mod_version not found!"
+  exit 1
+fi
+
+echo "Found mod properties: $mod_id-$mod_version"
+
+out_dir="publish-builds"
+mkdir -p "$out_dir"
+
+for mcdir in versions/*; do
+  [ -d "$mcdir" ] || continue
+
+  mcver="$(basename "$mcdir")"
+  jar="$mcdir/build/libs/$mod_id-$mod_version.jar"
+  out="$out_dir/$mod_id-$mod_version+mc$mcver.jar"
+
+  if [ -f "$jar" ]; then
+    cp "$jar" "$out"
+    echo "Copied mc$mcver"
+  else
+    echo "Skipping mc$mcver (not found)"
+  fi
+done

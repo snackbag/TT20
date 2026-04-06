d9e8e3e2917ac14ade2b47bb4bb7488df954a181
lemonnik <lemongamer6484@gmail.com>
2026-04-06 20:08:57 +0200

    26.1 support (a bit broken)

--- src/main/java/net/snackbag/tt20/mixin/world/ServerLevelMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/world/ServerLevelMixin.java
@@ -1,8 +1,6 @@
 package net.snackbag.tt20.mixin.world;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.server.IntegratedServer;
 import net.minecraft.server.level.ServerLevel;
 import net.snackbag.tt20.TT20;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,19 +10,14 @@ import org.spongepowered.asm.mixin.injection.At;
 public abstract class ServerLevelMixin {
     @ModifyExpressionValue(
             method = "tickTime",
+            //? >=26.1 {
+            /*at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/WritableLevelData;getGameTime()J")
+            *///?} else {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/WritableLevelData;getDayTime()J")
+            //?}
     )
     private long addMissingTicksToTime(long original) {
-        IntegratedServer server = Minecraft.getInstance().getSingleplayerServer();
-        boolean isLan = server != null && server.isPublished();
-
-        //? if >=1.20.1 {
-        boolean isPaused = Minecraft.getInstance().isSingleplayer() && Minecraft.getInstance().isPaused() && !isLan;
-        //?} else {
-        /*boolean isPaused = server.isSingleplayer() && Minecraft.getInstance().isPaused() && !isLan;
-        *///?}
-
-        if (!TT20.config.enabled() || !TT20.config.timeAcceleration() || isPaused) return original;
+        if (!TT20.config.enabled() || !TT20.config.timeAcceleration()) return original;
         return original + TT20.TPS_CALCULATOR.applicableMissedTicks();
     }
 }

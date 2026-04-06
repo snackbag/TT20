6d6b8a0909cc0439ccdcca01c50933f19fc33609
lemonnik <lemongamer6484@gmail.com>
2026-04-06 04:40:00 +0200

    Cross-loader client class

--- src/main/java/net/snackbag/tt20/TT20Client.java
+++ src/main/java/net/snackbag/tt20/TT20Client.java
@@ -1,11 +1,35 @@
 package net.snackbag.tt20;
 
+//? if fabric
 import net.fabricmc.api.ClientModInitializer;
 
-public class TT20Client implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
+//? if forge
+//import net.minecraftforge.fml.common.Mod;
+
+//? if neoforge {
+/*import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+*///?}
+
+//? if forge || neoforge
+//@Mod.EventBusSubscriber(modid = TT20.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class TT20Client
+        //? if fabric
+        implements ClientModInitializer
+
+    {
+
+    //? if fabric
+    @Override public void onInitializeClient()
+
+    //? if neoforge
+    //public TT20Client(IEventBus modBus)
+
+    //? if forge
+    //public TT20Client()
+
+    {
         TT20.LOGGER.warn("******* WARNING *******");
-        TT20.LOGGER.warn("(TT20) Mod is running on a client instead of server, this is not recommended and will lead to errors.");
+        TT20.LOGGER.warn("(TT20) Mod is running on a client instead of server, this is not recommended.");
     }
 }

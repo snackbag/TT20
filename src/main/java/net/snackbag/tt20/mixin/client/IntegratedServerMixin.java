3f4ec9f70213f12b5eac3f1888c10ae4b3e5ff3e
lemonnik <lemongamer6484@gmail.com>
2026-04-06 04:56:15 +0200

    loader-specific client-only annotations

--- src/main/java/net/snackbag/tt20/mixin/client/IntegratedServerMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/client/IntegratedServerMixin.java
@@ -1,7 +1,20 @@
 package net.snackbag.tt20.mixin.client;
 
+//? if fabric {
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+//?}
+
+//? if forge {
+/*import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+*///?}
+
+//? if neoforge {
+/*import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+*///?}
+
 import net.minecraft.client.server.IntegratedServer;
 import net.snackbag.tt20.TT20;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,7 +22,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+//? if fabric
 @Environment(EnvType.CLIENT)
+
+//? if forge || neoforge
+//@OnlyIn(Dist.CLIENT)
+
 @Mixin(IntegratedServer.class)
 public abstract class IntegratedServerMixin {
     @Inject(method = "halt", at = @At("HEAD"))

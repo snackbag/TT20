e17721a09bc94229ace38a7de25d87162414be76
lemonnik <lemongamer6484@gmail.com>
2026-04-06 05:17:39 +0200

    fix (neo)forge client class

--- src/main/java/net/snackbag/tt20/TT20Client.java
+++ src/main/java/net/snackbag/tt20/TT20Client.java
@@ -3,16 +3,30 @@ package net.snackbag.tt20;
 //? if fabric
 import net.fabricmc.api.ClientModInitializer;
 
-//? if forge
-//import net.minecraftforge.fml.common.Mod;
-
-//? if neoforge {
-/*import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.Mod;
+//? if forge {
+/*import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
 *///?}
 
-//? if forge || neoforge
+//? if neoforge {
+/*import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+*///?}
+
+//? if neoforge && <1.20.5
+//import net.neoforged.fml.common.Mod;
+
+//? if neoforge && >=1.20.5 <1.21.9 {
+/*import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.EventBusSubscriber.Bus;
+*///?}
+
+//? if forge || (neoforge && <1.20.5)
 //@Mod.EventBusSubscriber(modid = TT20.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+
+//? if neoforge && >=1.20.5 <1.21.9
+//@EventBusSubscriber(modid = TT20.MOD_ID, bus = Bus.MOD, value = Dist.CLIENT)
+
 public class TT20Client
         //? if fabric
         implements ClientModInitializer

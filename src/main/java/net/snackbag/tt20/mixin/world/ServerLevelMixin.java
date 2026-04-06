815650cdc9791a8fc9071df2bf192ef05cecdc83
lemonnik <lemongamer6484@gmail.com>
2026-04-06 16:48:16 +0200

    potential daytime advance on pause fix

--- src/main/java/net/snackbag/tt20/mixin/world/ServerLevelMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/world/ServerLevelMixin.java
@@ -1,6 +1,8 @@
 package net.snackbag.tt20.mixin.world;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.server.IntegratedServer;
 import net.minecraft.server.level.ServerLevel;
 import net.snackbag.tt20.TT20;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,7 +15,16 @@ public abstract class ServerLevelMixin {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/WritableLevelData;getDayTime()J")
     )
     private long addMissingTicksToTime(long original) {
-        if (!TT20.config.enabled() || !TT20.config.timeAcceleration()) return original;
+        IntegratedServer server = Minecraft.getInstance().getSingleplayerServer();
+        boolean isLan = server != null && server.isPublished();
+
+        //? if >=1.20.1 {
+        /*boolean isPaused = Minecraft.getInstance().isSingleplayer() && Minecraft.getInstance().isPaused() && !isLan;
+        //?} else {
+        */boolean isPaused = server.isSingleplayer() && Minecraft.getInstance().isPaused() && !isLan;
+        //?}
+
+        if (!TT20.config.enabled() || !TT20.config.timeAcceleration() || isPaused) return original;
         return original + TT20.TPS_CALCULATOR.applicableMissedTicks();
     }
 }

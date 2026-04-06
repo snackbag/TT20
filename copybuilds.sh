3312c69c8e34df9db85e241b17b1d66aa61c2614
lemonnik <lemongamer6484@gmail.com>
2026-04-06 05:20:49 +0200

    update copybuilds.sh

--- copybuilds.sh
+++ copybuilds.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-mod_id="$(grep -E '^mod_id=' gradle.properties | cut -d'=' -f2)"
+mod_name="$(grep -E '^mod_name=' gradle.properties | cut -d'=' -f2)"
 mod_version="$(grep -E '^mod_version=' gradle.properties | cut -d'=' -f2)"
 
 if [ -z "$mod_version" ]; then
@@ -9,7 +9,7 @@ if [ -z "$mod_version" ]; then
   exit 1
 fi
 
-echo "Found mod properties: $mod_id-$mod_version"
+echo "Found mod properties: $mod_name-$mod_version"
 
 out_dir="publish-builds"
 mkdir -p "$out_dir"
@@ -21,12 +21,12 @@ for mcdir in versions/*; do
 
   mcplatform="${dirname%%-*}"
   mcver="${dirname#*-}"
-  jar="$mcdir/build/libs/$mod_id-$mod_version+mc$mcver-$mcplatform.jar"
+  jar="$mcdir/build/libs/$mod_name-$mod_version+mc$mcver-$mcplatform.jar"
 
   if [ -f "$jar" ]; then
     cp "$jar" "$out_dir/"
-    echo "Copied $mcplatform-$mcver"
+    echo "Copied $mod_name-$mod_version+mc$mcver-$mcplatform"
   else
-    echo "Skipping $mcplatform-$mcver (not found)"
+    echo "Skipping $mod_name-$mod_version+mc$mcver-$mcplatform (not found)"
   fi
 done

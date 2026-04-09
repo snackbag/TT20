ba1c4336679c4b4ad16e721a5e15c1c60280032a
lemonnik <lemongamer6484@gmail.com>
2026-04-10 01:03:19 +0200

    fix bow & crossbow mixin mappings

--- src/main/java/net/snackbag/tt20/mixin/item/BowItemMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/item/BowItemMixin.java
@@ -1,7 +1,7 @@
 package net.snackbag.tt20.mixin.item;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import net.minecraft.item.BowItem;
+import net.minecraft.world.item.BowItem;
 import net.snackbag.tt20.TT20;
 import net.snackbag.tt20.util.TPSCalculator;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(BowItem.class)
 public class BowItemMixin {
-    @ModifyReturnValue(method = "getPullProgress", at = @At("RETURN"))
+    @ModifyReturnValue(method = "getPowerForTime", at = @At("RETURN"))
     private static float bowProgress(float original) {
         if (!TT20.config.enabled() || !TT20.config.bowAcceleration()) return original;
 

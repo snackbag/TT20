f4d48dcfc1491b70e4d141d36b8a4bcf7d216ed3
lemonnik <lemongamer6484@gmail.com>
2026-04-11 00:54:23 +0200

    fix command register

--- src/main/java/net/snackbag/tt20/mixin/MinecraftServerMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/MinecraftServerMixin.java
@@ -1,10 +1,7 @@
 package net.snackbag.tt20.mixin;
 
-import com.mojang.brigadier.CommandDispatcher;
-import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.server.MinecraftServer;
 import net.snackbag.tt20.TT20;
-import net.snackbag.tt20.command.MainCommand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,12 +14,5 @@ public class MinecraftServerMixin {
     @Inject(method = "tickServer", at = @At("TAIL"))
     private void tt20$serverTick(BooleanSupplier hasTimeLeft, CallbackInfo ci) {
         TT20.TPS_CALCULATOR.onTick();
-    }
-
-    @Inject(method = "loadLevel", at = @At("TAIL"))
-    private void tt20$onServerStart(CallbackInfo ci) {
-        MinecraftServer server = (MinecraftServer)(Object) this;
-        CommandDispatcher<CommandSourceStack> dispatcher = server.getCommands().getDispatcher();
-        MainCommand.register(dispatcher);
     }
 }

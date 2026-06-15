e17b2fc05c714454be0baf130b31e578b8bde671
lemonnik <lemongamer6484@gmail.com>
2026-06-16 00:31:18 +0200

    bump fern to 1.2.0

---
+++ fern-updater.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+OLD_PREFIX="0.1.0"
+NEW_PREFIX="1.2.0"
+
+find versions -type f -name "gradle.properties" | while read -r file; do
+    sed -i "s/\(fern_version=\)${OLD_PREFIX}/\1${NEW_PREFIX}/" "$file"
+    echo "Updated $file"
+done

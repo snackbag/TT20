773ac3546fc1e14546ee55176032493bc9ddc354
Lemonnik6484 <lemongamer6484@gmail.com>
2026-06-18 16:24:59 +0200

    fern 1.2.0 -> 1.3.0

--- fern-updater.sh
+++ fern-updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-OLD_PREFIX="0.1.0"
-NEW_PREFIX="1.2.0"
+OLD_PREFIX="1.2.0"
+NEW_PREFIX="1.3.0"
 
 find versions -type f -name "gradle.properties" | while read -r file; do
     sed -i "s/\(fern_version=\)${OLD_PREFIX}/\1${NEW_PREFIX}/" "$file"
